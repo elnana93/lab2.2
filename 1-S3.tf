44a4b7a0095942a163825020a8d5151bc7480aaa
elnana93 <e5leadsusa@gmail.com>
2025-11-23 17:10:49 -0800

    Nov 23 Final

--- 1-S3.tf
+++ 1-S3.tf
@@ -2,6 +2,7 @@
 
     resource "aws_s3_bucket" "my_bucket" {
       bucket = "e5tech" # Replace with a globally unique bucket name
+      force_destroy = true
       #acl    = "private" # Set the desired Access Control List (e.g., private, public-read)
 
       tags = {
@@ -9,3 +10,6 @@
         Environment = "Development"
       }
     }
+
+
+    #/  aws s3 cp /Users/elnana93/assets/e5foto.jpg s3://e5tech/  #/- this is to upload your stuff to s3 from the terminal/vscode to the cansole
